61ca03c7402525e8aee86e84fbd36f6539f2878c
Maher Hawash <gmhawash@gmail.com>
2014-01-05 16:40:05 -0500

    initial

---
+++ cookbooks/op/recipes/default.rb
@@ -0,0 +1,47 @@
+# install packages
+
+package 'build-essential'
+package 'git-core'
+package 'curl'
+package 'wget'
+package 'openssl'
+package 'libssl-dev'
+package 'libopenssl-ruby'
+package 'ruby-dev'
+
+# install ack
+package 'ack-grep'
+execute 'ln -sf /usr/bin/ack-grep /usr/bin/ack'
+
+
+#install ruby-shadow - needed to set user password on the 'user' node
+# ref: http://docs.opscode.com/resource_user.html
+package 'libshadow-ruby1.8'
+execute 'gem install ruby-shadow'
+
+package 'vnc4server'
+package 'firefox'
+package 'xorg'
+package 'gdm'
+package 'gnome-session'
+
+# setup postgresql 9.3
+file '/etc/apt/sources.list.d/pgdg.list' do
+  content 'deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main'
+  action :create
+end
+
+execute 'wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -'
+execute 'apt-get update'
+execute 'yes | apt-get install postgresql-9.3 postgresql-common postgresql-contrib'
+
+user 'mmh' do
+  action :create
+  username 'mmh'
+  password '$1$5wFLHaE6$3G3NqMTMeYNRVmqF.IoUo0'
+  shell '/bin/bash'
+  home '/home/mmh'
+  system true
+  supports :manage_home => true
+end
+
